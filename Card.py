9f92de10ef1e0c3476c23bda4f9a4a39fb855b13
RalphGorham05 <Rgorham1@gaels.iona.edu>
2015-06-02 15:59:08 -0400

    minor

---
+++ Card.py
@@ -0,0 +1,6 @@
+class Card:
+    def__init__(self, name):
+        self.name = name
+        self.stars = 0
+        
+        
--- throwdown.py
+++ throwdown.py
@@ -5,18 +5,38 @@ Throwdown
 
 from random import randint
 
-p1_number = input('Enter a number between 1-100 Player 1: ')
-p2_number = input('Enter a number between 1-100 Player 2: ')
+
+def pick_numbers():
+    player_numbers = []
+    p1_number = input('Enter a number between 1-100 Player 1: ')
+    if not (1 <= p1_number <= 100):
+        print 'invalid number, one more chance to pick number'
+        p1_number = input('Enter a number between 1-100 Player 1: ')
+
+    player_numbers.append(p1_number)
+        
+        
+    p2_number = input('Enter a number between 1-100 Player 2: ')
+    if not (1 <= p2_number <= 100):
+        print 'invalid number, one more chance to pick number'
+        p2_number = input('Enter a number between 1-100 Player 2: ')
+
+    player_numbers.append(p2_number)
+
+    return player_numbers
+
+
+numbers = pick_numbers()
 
 home = randint(1,100)
-print home
+#print home
 
-if (abs(p1_number - home) < abs(p2_number - home)):
+if (abs(numbers[0] - home) < abs(numbers[1] - home)):
     print 'p1 is home'
 
-elif(abs(p1_number - home) == abs(p2_number - home)):
-    print 'do over'
+elif(abs(numbers[0] - home) == abs(numbers[1] - home)):
+    print 'What are the odds!'
+    pick_numbers()
     
 else:
     print 'p2 is home'
-
